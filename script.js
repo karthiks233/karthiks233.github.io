b6b50cb78b6bff8b262ff5637b556c314083ed23
Karthik Raja <67165351+karthiks233@users.noreply.github.com>
2023-12-18 00:14:04 -0500

    Update script.js with sentiment

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function result(){
+document.getElementById('result').innerText = ' Sentiment';
+}
+
+$(document).ready(function () {
+  $('#myForm').submit(function (event) {
+      // Prevent the default form submission
+      event.preventDefault();
+
+      // Get form data
+      var formData = $(this).serialize();
+
+      // Perform AJAX submission
+      $.ajax({
+          type: 'POST',
+          url: '{{ url_for('submit_form') }}',
+          data: formData,
+          success: function (data) {
+              // Update your page with the result if needed
+              // Example: $('#resultContainer').html(data);
+          }
+      });
+  });
+});
