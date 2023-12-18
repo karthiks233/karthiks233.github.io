32826f85455f125ed38325676266c60521c07a0e
Karthik Raja <67165351+karthiks233@users.noreply.github.com>
2023-12-18 00:28:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,27 +6,4 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-function result(){
-document.getElementById('result').innerText = ' Sentiment';
-}
 
-$(document).ready(function () {
-  $('#myForm').submit(function (event) {
-      // Prevent the default form submission
-      event.preventDefault();
-
-      // Get form data
-      var formData = $(this).serialize();
-
-      // Perform AJAX submission
-      $.ajax({
-          type: 'POST',
-          url: '{{ url_for('submit_form') }}',
-          data: formData,
-          success: function (data) {
-              // Update your page with the result if needed
-              // Example: $('#resultContainer').html(data);
-          }
-      });
-  });
-});
