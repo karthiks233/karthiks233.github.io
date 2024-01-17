4e93dac11da9770aed84f9754660defadbf43a38
Karthik Raja <67165351+karthiks233@users.noreply.github.com>
2024-01-17 01:38:33 -0500

    Update script.js
    adding transition my name

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// document.getElementById('result').innerText = '{{ result }}';
+document.addEventListener("DOMContentLoaded", function() {
+  // Run this function when the DOM is fully loaded
 
+// Get the logo element
+var logo = document.querySelector('#profile .title');
+
+// Add a class to make the logo visible with a transition
+logo.classList.add('visible');
+
+});
 
